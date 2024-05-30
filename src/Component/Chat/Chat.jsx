8c1c14eba6ba0e3e693db38eb675a7816d37446b
Sushant Lama <sushantlama732@gmail.com>
2024-05-30 02:32:52 -0700

    include message box

--- src/Component/Chat/Chat.jsx
+++ src/Component/Chat/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Chat.scss'
+import ChatMessage from '../ChatMessage/ChatMessage'
 
 const Chat = () => {
   return (
@@ -19,11 +20,29 @@ const Chat = () => {
 
             
 
-        <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-        <span>Sushant Lama</span>
-        <p>Lorem ipsum dolor sit amet consectetur...</p>
+<img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+<span>Sushant Lama</span>
+<p>Lorem ipsum dolor sit amet consectetur...</p>
 
-        </div>
+</div>
+<div className='message'>
+
+            
+
+<img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+<span>Sushant Lama</span>
+<p>Lorem ipsum dolor sit amet consectetur...</p>
+
+</div>
+<div className='message'>
+
+            
+
+<img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+<span>Sushant Lama</span>
+<p>Lorem ipsum dolor sit amet consectetur...</p>
+
+</div>
         <div className='message'>
 
             
@@ -42,8 +61,18 @@ const Chat = () => {
         <p>Lorem ipsum dolor sit amet consectetur...</p>
 
         </div>
+        <div className='message'>
+
+            
+
+        <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <span>Sushant Lama</span>
+        <p>Lorem ipsum dolor sit amet consectetur...</p>
+
         </div>
-        <div className="chat">
+        </div>
+        <div className="chats">
+            <ChatMessage/>
 
         </div>
     </div>
---
+++ src/Component/ChatMessage/ChatMessage.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './ChatMessage.scss'
+const ChatMessage = () => {
+  return (
+    <>
+    <div className='top'>
+    <div className='user'>
+    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      
+      <span>Sushant Lama</span>
+    </div>
+    <p>X</p>
+    </div>
+     <div className='center'>
+     <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+
+     </div>
+      <div className='bottom'>
+<textarea name="" id=""></textarea><button>Send</button>
+
+      </div>
+      </>
+  )
+}
+
+export default ChatMessage
